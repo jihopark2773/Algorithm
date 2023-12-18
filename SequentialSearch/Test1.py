67e5167071372cc633fecfbc7e819e111692cd7d
Joe <72645835+jihopark2773@users.noreply.github.com>
2023-12-18 19:03:25 +0900

    Update Test1.py
    Add Test Case

--- SequentialSearch/Test1.py
+++ SequentialSearch/Test1.py
@@ -6,4 +6,10 @@ def seqsearch(n, S, x):
     location = 0
   return location
 
+## Test Case1
 seqsearch(5, [1,2,3,4,5], 5)
+
+## Test Case2
+S = [1, 11, 13, 14, 15]
+x = 14
+seqsearch(len(S)-1, S, x)
